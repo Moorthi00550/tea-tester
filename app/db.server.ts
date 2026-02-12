da472495cf4c0db13f6982d51f70f74288aece39
Moorthi00550 <moorthimoorthi81841@gmail.com>
2026-02-12 18:55:58 +0530

    fix: changed the sqllite to psql

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,3 +1,20 @@
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var prismaGlobal: PrismaClient;
+// }
+
+// if (process.env.NODE_ENV !== "production") {
+//   if (!global.prismaGlobal) {
+//     global.prismaGlobal = new PrismaClient();
+//   }
+// }
+
+// const prisma = global.prismaGlobal ?? new PrismaClient();
+
+// export default prisma;
+
 import { PrismaClient } from "@prisma/client";
 
 declare global {
@@ -5,12 +22,18 @@ declare global {
   var prismaGlobal: PrismaClient;
 }
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
-  }
-}
+const prisma =
+  global.prismaGlobal ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 
-const prisma = global.prismaGlobal ?? new PrismaClient();
+if (process.env.NODE_ENV !== "production") {
+  global.prismaGlobal = prisma;
+}
 
 export default prisma;
